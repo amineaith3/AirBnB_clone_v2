2f02c1983a8383cbb598fe309a21628c7d7a98cf
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2024-01-17 22:51:55 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -15,3 +15,4 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=True)
 
     places = relationship("Place", cascade="all, delete-orphan", back_populates="user")
+    reviews = relationship("Review", cascade="all, delete-orphan", back_populates="user")
