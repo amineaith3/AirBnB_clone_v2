b3943f324f26a82a25ae6ef1060dd69abdb7b33b
sakuun_aah <91127128+amineaith3@users.noreply.github.com>
2024-01-17 22:54:05 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" Amenity Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Amenity class to store amenity information """
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
