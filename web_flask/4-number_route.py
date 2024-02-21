66c23dec7f15d999ec9c59f3034b1bcd36717f94
fatizaheracompus <fatimaaitbrahim18@gmail.com>
2024-02-21 14:24:10 -0800

    r

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,52 +1,52 @@
 #!/usr/bin/python3
-"""script that start a Flask web application"""
+"""script that starts a Flask web application"""
 
 
 # import Flask class from flask module
 from flask import Flask
 
-# creates instance call app of the class by pass the __name__ variable
-appc = Flask(__name__)
-appc.url_map.strict_slashes = False
+# create an instance called app of the class by passong the __name__ variable
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@appc.route('/')
+@app.route('/')
 def index():
     """display "Hello HBNB!"
 
-    Return:
-        str: text on the indexe page
+    Returns:
+        str: text on the index page
     """
     return 'Hello HBNB!'
 
 
-@appc.route('/hbnb')
+@app.route('/hbnb')
 def hbnb_route():
     """display "HBNB"
 
-    Return:
-        str: texte on the page
+    Returns:
+        str: text on the page
     """
     return 'HBNB'
 
 
-@appc.route('/c/<text>')
+@app.route('/c/<text>')
 def c_route(text):
-    """displaye "C", follow by the value of the text variable
+    """display "C", followed by the value of the text variable
 
     Args:
-        text (str): text to be serve on the page
+        text (str): text to be served on the page
 
-    Return:
+    Returns:
         str: text on the page
     """
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@appc.route('/python', defaults={'text': 'is cool'})
-@appc.route('/python/<text>')
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
 def python_route(text):
-    """displays "Python", followed by the value of the text variable
+    """display "Python", followed by the value of the text variable
 
     Args:
         text (str): text to be served on the page
@@ -57,18 +57,18 @@ def python_route(text):
     return 'Python {}'.format(text.replace('_', ' '))
 
 
-@appc.route('/number/<int:n>')
+@app.route('/number/<int:n>')
 def number_route(n):
-    """display "n is a number" only if n is  integer
+    """display "n is a number" only if n is an integer
 
     Args:
-        n (integer): number to be display on page
+        n (integer): number to be displayed on page
 
-    Return:
+    Returns:
         str: text on the page
     """
     return '{} is a number'.format(n)
 
 
 if __name__ == '__main__':
-    appc.run(debug=True)
+    app.run(debug=True)
